55f810bf4579de77e37298a48a24c3a415e2731f
Yasir Haq <yasirrhaq@users.noreply.github.com>
2019-05-12 20:19:49 +0700

    Create ParticleTimer Script

---
+++ Assets/Scripts/ParticleTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTimer : MonoBehaviour
+{
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer>0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if (timer <=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
